19f79e24a1ac26cd336d30a4d279efbe6f664ef8
Tejj..! <80447449+steja578@users.noreply.github.com>
2025-11-19 15:24:52 -0500

    Delete script_js.js

--- script_js.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll', () => {
-  document.querySelectorAll('.glass-card').forEach((card) => {
-    const rect = card.getBoundingClientRect();
-    if (rect.top < window.innerHeight - 50) {
-      card.style.opacity = 1;
-      card.style.transform = 'translateY(0)';
-    }
-  });
-});
